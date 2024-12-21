aab2298c128a6e8fadf87bb72034196da498eda7
n0xstella <bedkent1990@gmail.com>
2024-12-21 13:39:03 -0500

    updated license and main

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from constants import *
 
 def main():
     print('"Starting asteroids!"')
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(color="black")
+        pygame.display.flip()
 
 
 
